8874c3f3642bd7182f724b1c113f83188bdc7c56
Chem O'Dun <chemodun@gmail.com>
2025-10-01 02:37:29 +0300

    feat: Initial pre-release for Gate Manager UI and functionality
    - Introduced XML files for various languages including French, Spanish, Italian, Czech, English, Polish, German, Portuguese, Japanese, Korean, Chinese, Bengali, Turkish, and Ukrainian.
    - Each language file contains localized strings for the Gate Manager interface.
    - Implemented the core functionality of the Gate Manager in Lua, allowing players to create, connect, and disconnect gates within the game.
    - Added UI integration for the Gate Manager, enabling interaction through the game's menu system.
    - Included error handling and success reporting mechanisms for user commands related to gate management.

---
+++ README.md
@@ -0,0 +1,154 @@
+# Gate Manager: connecting ... sectors
+
+Adds map context actions to build, connect and destroy player-defined Jump Gates and Accelerators.
+
+## Compatibility
+
+Compatible with `X4: Foundations 7.60` and upper. At least it written and tested with versions `7.60` and `8.00`.
+
+## Features
+
+- Build Jump Gates and Accelerators in the Galaxy Map view
+- Connect Jump Gates and Accelerators to create travel routes (is only possible to connect Gates to Gates or Accelerators to Accelerators)
+- Disconnect Jump Gates and Accelerators (player-built ones as well as original ones)
+- Destroy unwanted Jump Gates and Accelerators (only player-built ones)
+- For Gates used the `props_gates_anc_gate_macro` model, for Accelerators used the `props_gates_orb_accelerator_01_macro` model
+
+## Download
+
+You can download the latest version via Steam client - [Gate Manager](https://steamcommunity.com/sharedfiles/filedetails/?id=3577926900)
+Or you can do it via the Nexus Mods - [Gate Manager](https://www.nexusmods.com/x4foundations/mods/1842)
+
+## Dependencies
+
+As it uses context menu in the Galaxy Map view, it requires the `Mod Support APIs` by [SirNukes](https://next.nexusmods.com/profile/sirnukes?gameId=2659) to be installed and enabled. Version `1.93` and upper is required.
+
+- It is available via Steam - [SirNukes Mod Support APIs](https://steamcommunity.com/sharedfiles/filedetails/?id=2042901274)
+- Or via the Nexus Mods - [Mod Support APIs](https://www.nexusmods.com/x4foundations/mods/503)
+
+## How to use
+
+After installation, you can use the context menu in the Galaxy Map view to build, connect, disconnect and destroy Jump Gates and Accelerators.
+
+### Build
+
+- Open the Galaxy Map view
+- Right-click in the sector on position where you want to build a Jump Gate or an Accelerator
+- Select the `Build Jump Gate` or `Build Accelerator` option from the context menu
+- The Jump Gate or Accelerator will be built in the selected sector exactly in the selected position.
+
+![Build Jump Gate or Accelerator](docs/images/context_menu_build.png)
+
+By default, the Jump Gate or Accelerator will be oriented from "South" to "North". If you want to change the orientation, please read the appropriate section below.
+
+### Connect
+
+Applicable as for newly built objects as well as for original ones.
+
+- Open the Galaxy Map view
+- Right-click on the Jump Gate or Accelerator you want to connect. (It must be already built and inactive, i.e. not currently connected.)
+- Select `Select [Inactive Jump Gate|Accelerator] to connect` option from the context menu
+- This object will be marked as selected
+- Right-click on the Jump Gate or Accelerator you want to connect to
+- Select `Connect [Sector] to [Sector]` option from the context menu
+- Appropriate connection will be created between the two objects
+
+#### Inactive object (not currently connected)
+
+![Context menu for inactive object](docs/images/context_menu_for_inactive_object.png)
+
+#### Selected object
+
+![There is how the selected object will look like](docs/images/selected_object.png)
+
+#### Context menu when other object is selected to connect to
+
+![Context menu if other object selected to connect](docs/images/context_menu_if_other_object_selected.png)
+
+#### Limitations
+
+- It is not possible to connect already connected objects
+- It is only possible to connect Jump Gates to Jump Gates or Accelerators to Accelerators
+- It is only possible to connect objects in different sectors
+
+##### Deselect
+
+If you already marked some object as selected. to make possible to select another one, you need to deselect the already selected object.
+
+- Right-click on the already selected Jump Gate or Accelerator. (It must be already marked as selected if you not changed the selection.)
+- Select `Deselect` option from the context menu
+
+![Context menu on selected object](docs/images/context_menu_on_selected_object.png)
+
+### Disconnect
+
+- Open the Galaxy Map view
+- Right-click on the Jump Gate or Accelerator you want to disconnect. (It must be already built and active, i.e. currently connected.)
+- Select `Disconnect` option from the context menu
+- The connection will be removed and the objects will be inactive
+- You can connect them again, or destroy (if it was a player-built one) them if you don't need them anymore
+
+![Context menu on active object](docs/images/context_menu_on_active_object.png)
+
+### Destroy
+
+- Open the Galaxy Map view
+- Right-click on the Jump Gate or Accelerator you want to destroy. (It must be built by the player and inactive, i.e. not currently connected.)
+- Select `Destroy ...` option from the context menu
+- The object will be removed from the Galaxy Map
+
+**Warning:** Some original gates and accelerators can be identified as player-built ones, due to some specifics. So please be careful when destroying the objects.
+
+![Context menu for inactive object](docs/images/context_menu_for_inactive_object.png)
+
+### Change orientation
+
+By default, the Jump Gate or Accelerator will be oriented from "South" to "North" for the current map orientation.
+It is mean, that you will "enter" the object from "South" to "North". But exiting the opposite object will be from "North" to "South".
+
+So, if you want to make a route more effective, please orient the Gates/Accelerators accordingly, i.e. to have the "entrance" in the direction of the destination sector. From both sides of the connection.
+
+Please take in account, that the orientation is dependent on the current map orientation. By default, the map orientation is "South" to "North", i.e. "South" is at the bottom of the map and "North" is at the top of the map.
+I.e. if you want to build the object oriented from "West" to "East", i.e. target sector is on a right hand for current, you need to rotate the map to have "West" at the bottom of the map (for source sector).
+And make it opposite for the target sector, i.e. rotate the map to have "East" at the bottom of the map.
+All of these is valid if you place the object in the closest border to opposite sector.
+
+To reset map orientation, please use the '.' hotkey to focus map on your current position. In this case orientation will be "South" to "North" for a whole map.
+
+## Logging
+
+### Notifications
+
+All your actions will be shown in notification window (not really useful in the Map view).
+
+![Notifications](docs/images/notifications.png)
+
+### Logbook
+
+In addition, all your actions will be logged in the logbook (accessible via the Player Information menu).
+
+![Logbook records](docs/images/logbook.png)
+
+## Video
+
+[Video demonstration of the Gate Manager. Short one](https://www.youtube.com/watch?v=bpB_Y5TShgw)
+[Video demonstration of the Gate Manager. Full one](https://www.youtube.com/watch?v=4TyMagNIVC0)
+
+## Credits
+
+- Author: Chem O`Dun, on [Nexus Mods](https://next.nexusmods.com/profile/ChemODun/mods?gameId=2659) and [Steam Workshop](https://steamcommunity.com/id/chemodun/myworkshopfiles/?appid=392160)
+- *"X4: Foundations"* is a trademark of [Egosoft](https://www.egosoft.com).
+
+## Acknowledgements
+
+- [EGOSOFT](https://www.egosoft.com) - for the game itself (In fact - for the series of games)!
+- Enormous big thank to [Forleyor](https://github.com/Forleyor), for his help with Lua! For make me curious about it and for his patience! Without him I will never start to make this mod!
+- Thanks to all members of the [X4 modding channel](https://discord.com/channels/337098290917146624/502057640877228042) on [Egosoft Discord](https://discord.com/invite/zhs8sRpd3m).
+- Thanks to [SirNukes](https://next.nexusmods.com/profile/sirnukes?gameId=2659) for his awesome `Mod Support APIs` without which this mod would not be possible!
+
+## Change Log
+
+### [1.00] - 2025-10-01
+
+- Added
+  - Initial public version
---
+++ ui/gate_manager.lua
@@ -0,0 +1,350 @@
+local ffi = require("ffi")
+local C = ffi.C
+
+ffi.cdef [[
+  typedef uint64_t UniverseID;
+
+  typedef struct {
+    float x;
+    float y;
+    float z;
+    float yaw;
+    float pitch;
+    float roll;
+  } UIPosRot;
+
+  UniverseID  GetPlayerID(void);
+	UIPosRot    GetPositionalOffset(UniverseID positionalid, UniverseID spaceid);
+  void        SpawnObjectAtPos(const char* macroname, UniverseID sectorid, UIPosRot offset);
+	UniverseID  SpawnObjectAtPos2(const char* macroname, UniverseID sectorid, UIPosRot offset, const char* ownerid);
+	void        SetObjectSectorPos(UniverseID objectid, UniverseID sectorid, UIPosRot offset);
+  void        SetObjectForcedRadarVisible(UniverseID objectid, bool value);
+  void        SetKnownTo(UniverseID componentid, const char* factionid);
+  bool        IsComponentClass(UniverseID componentid, const char* classname);
+  void        AddGateConnection(UniverseID gateid, UniverseID othergateid);
+  void        RemoveGateConnection(UniverseID gateid, UniverseID othergateid);
+	void        SetFocusMapComponent(UniverseID holomapid, UniverseID componentid, bool resetplayerpan);
+	void        SetSelectedMapComponent(UniverseID holomapid, UniverseID componentid);
+	void        SetSelectedMapComponents(UniverseID holomapid, UniverseID* componentids, uint32_t numcomponentids);
+	bool        SetSofttarget(UniverseID componentid, const char*const connectionname);
+]]
+
+local GateManager = {
+  playerId = 0,
+}
+
+local Lib = require("extensions.sn_mod_support_apis.ui.Library")
+
+local function debugTrace(message)
+  local text = "Gate Manager: " .. message
+  if type(DebugError) == "function" then
+    DebugError(text)
+  else
+    print(text)
+  end
+end
+
+local function getPlayerId()
+  local current = C.GetPlayerID()
+  if current == nil or current == 0 then
+    return
+  end
+
+  local converted = ConvertStringTo64Bit(tostring(current))
+  if converted ~= 0 and converted ~= GateManager.playerId then
+    debugTrace("updating player_id to " .. tostring(converted))
+    GateManager.playerId = converted
+  end
+end
+
+local function toUniverseId(value)
+  if value == nil then
+    return 0
+  end
+
+  if type(value) == "number" then
+    return value
+  end
+
+  local idStr = tostring(value)
+  if idStr == "" or idStr == "0" then
+    return 0
+  end
+
+  return ConvertStringTo64Bit(idStr)
+end
+
+local function RevertAndApplyMapRotation()
+  local pi = math.pi
+  local twoPi = 2 * pi
+  local menu = Lib.Get_Egosoft_Menu("MapMenu")
+  debugTrace("MapMenu is " .. tostring(menu))
+  if menu and menu.holomap and (menu.holomap ~= 0) then
+    local mapstate = ffi.new("HoloMapState")
+    C.GetMapState(menu.holomap, mapstate)
+    debugTrace(string.format("MapState: pitch %f, yaw %f, roll %f", mapstate.offset.pitch, mapstate.offset.yaw,
+      mapstate.offset.roll))
+    -- modulo in Lua can be tricky with negatives, so use this pattern
+    angle = (mapstate.offset.yaw + pi) % twoPi
+    if angle > pi then
+        angle = angle - twoPi
+    end
+    return angle
+  else
+    debugTrace("No MapMenu or holomap, returning pi" .. tostring(pi))
+    return pi
+  end
+end
+
+local function toPosRot(raw)
+  local posRot = ffi.new("UIPosRot")
+  if raw then
+    posRot.x = raw.x or 0.0
+    posRot.y = raw.y or 0.0
+    posRot.z = raw.z or 0.0
+    posRot.yaw = RevertAndApplyMapRotation() * 180.0 / math.pi
+    posRot.pitch = 0.0
+    posRot.roll = 0.0
+  end
+  return posRot
+end
+
+local function recordResult(data)
+  debugTrace("recordResult called for command ".. tostring(data and data.command) .. " with result " .. tostring(data and data.result))
+  if GateManager.playerId ~= 0 then
+    local payload = data or {}
+    SetNPCBlackboard(GateManager.playerId, "$gateManagerResult", payload)
+    AddUITriggeredEvent("Gate_Manager_Interface", "onResult")
+  end
+end
+
+local function reportError(data)
+  local data = data or {}
+  data.result = "error"
+  recordResult(data)
+
+  local message = "Gate Manager error"
+  if data.info then
+    message = message .. ": " .. tostring(data.info)
+  end
+  if data.detail then
+    message = message .. " (" .. tostring(data.detail) .. ")"
+  end
+
+  DebugError(message)
+end
+
+local function reportSuccess(data)
+  data = data or {}
+  data.result = "success"
+  recordResult(data)
+end
+
+
+local function getArgs()
+  if GateManager.playerId == 0 then
+    debugTrace("getArgs unable to resolve player id")
+  else
+    local list = GetNPCBlackboard(GateManager.playerId, "$gateManagerArgs")
+    if type(list) == "table" then
+      debugTrace("getArgs retrieved " .. tostring(#list) .. " entries from blackboard")
+      local args = list[#list]
+      SetNPCBlackboard(GateManager.playerId, "$gateManagerArgs", nil)
+      return args
+    elseif list ~= nil then
+      debugTrace("getArgs received non-table payload of type " .. type(list))
+    else
+      debugTrace("getArgs found no blackboard entries for player " .. tostring(GateManager.playerId))
+    end
+  end
+  return nil
+end
+
+local function gate_destination_id(id)
+  local destination = GetComponentData(id, "destination")
+  if destination == nil then
+    return 0
+  end
+  local destinationId = toUniverseId(destination)
+  if destinationId == nil then
+    return 0
+  end
+  return destinationId
+end
+
+local function gate_has_connection(id)
+  return gate_destination_id(id) ~= 0
+end
+
+function GateManager.CreateGate(args)
+  debugTrace("CreateGate called in sector " .. GetComponentData(args.sector, "macro"))
+  local sector = toUniverseId(args.sector)
+  local macro = args.macro
+  if sector == 0 then
+    args.info = "MissingSector"
+    reportError(args)
+    return
+  end
+  if not macro or macro == "" then
+    args.info = "MissingMacro"
+    reportError(args)
+    return
+  end
+
+  local menu = Lib.Get_Egosoft_Menu("MapMenu")
+  debugTrace("MapMenu is " .. tostring(menu))
+  if menu and menu.holomap and (menu.holomap ~= 0) then
+    local mapstate = ffi.new("HoloMapState")
+    C.GetMapState(menu.holomap, mapstate)
+    debugTrace(string.format("MapState: pitch %f, yaw %f, roll %f", mapstate.offset.pitch, mapstate.offset.yaw,
+      mapstate.offset.roll))
+  end
+
+  local posRot = toPosRot(args.offset)
+  debugTrace(string.format("Spawning gate at x=%f, y=%f, z=%f, yaw=%f, pitch=%f, roll=%f", posRot.x, posRot.y,
+    posRot.z, posRot.yaw, posRot.pitch, posRot.roll))
+  local object = C.SpawnObjectAtPos2(macro, sector, posRot, "ownerless")
+
+  if object == nil then
+    args.info = "SpawnFailed"
+    reportError(args)
+    return
+  end
+  C.SetKnownTo(object, "player")
+  debugTrace("SpawnObjectAtPos returned object " .. GetComponentData(ConvertStringToLuaID(tostring(object)), "name"))
+
+  args.info = "GateCreated"
+  args.gate = ConvertStringToLuaID(tostring(object))
+  args.sector = args.sector
+  reportSuccess(args)
+end
+
+function GateManager.ConnectGates(args)
+  local gateSource = toUniverseId(args.gateSource)
+  local gateTarget = toUniverseId(args.gateTarget)
+  local sectorSource = GetComponentData(gateSource, "sector")
+  local sectorTarget = GetComponentData(gateTarget, "sector")
+
+  if gateSource == 0 or gateTarget == 0 or gateSource == gateTarget then
+    args.info = "InvalidGateIDs"
+    reportError(args)
+    return
+  end
+
+  if not C.IsComponentClass(gateSource, "gate") or not C.IsComponentClass(gateTarget, "gate") then
+    args.info = "NotGate"
+    reportError(args)
+    return
+  end
+
+  if gate_has_connection(gateSource) or gate_has_connection(gateTarget) then
+    args.info = "GateAlreadyConnected"
+    reportError(args)
+    return
+  end
+
+  C.AddGateConnection(gateSource, gateTarget)
+  local menu = Lib.Get_Egosoft_Menu("MapMenu")
+  debugTrace("MapMenu is " .. tostring(menu))
+  if menu and menu.holomap and (menu.holomap ~= 0) then
+    menu.selectedcomponents = {}
+  end
+  debugTrace(string.format("Connected gates %s (%s) <-> %s (%s)", tostring(gateSource), sectorSource,
+    tostring(gateTarget),
+    sectorTarget))
+  args.info = "GatesConnected"
+  reportSuccess(args)
+end
+
+function GateManager.DisconnectGate(args)
+  local gateSource = toUniverseId(args.gateSource)
+  local sectorSource = GetComponentData(gateSource, "sector")
+  local gateTarget = toUniverseId(args.gateTarget)
+  local sectorTarget = GetComponentData(gateTarget, "sector")
+
+  if gateSource == 0 then
+    args.info = "InvalidGateID"
+    reportError(args)
+    return
+  end
+
+  if not C.IsComponentClass(gateSource, "gate") then
+    args.info = "NotGate"
+    reportError(args)
+    return
+  end
+
+  if gateTarget == 0 then
+    args.info = "NotConnected"
+    reportError(args)
+    return
+  end
+
+  C.RemoveGateConnection(gateSource, gateTarget)
+
+  debugTrace(string.format("Disconnected gate %s (%s) from %s (%s)", tostring(gateSource), sectorSource,
+    tostring(gateTarget), sectorTarget))
+
+  args.info = "GateDisconnected"
+  reportSuccess(args)
+end
+
+function GateManager.MarkGateOnMap(args)
+  local gate = tostring(args.gate)
+  if not gate or gate == "" then
+    reportError({ info = "InvalidGateID" })
+    return
+  end
+
+  local menu = Lib.Get_Egosoft_Menu("MapMenu")
+  debugTrace("MapMenu is " .. tostring(menu) .. " for gate " .. gate)
+  if menu and menu.holomap and (menu.holomap ~= 0) then
+    menu.selectedcomponents = {}
+    if (args.command == "unmark_gate") then
+      args.info = "GateUnmarked"
+    else
+      args.info = "GateMarked"
+      menu.selectedcomponents[gate] = true
+    end
+    menu.refreshInfoFrame()
+  else
+    args.info = "NoMap"
+    reportError(args)
+    return
+  end
+
+  reportSuccess(args)
+end
+
+function GateManager.HandleCommand(_, _)
+  local args = getArgs()
+  if not args or type(args) ~= "table" then
+    debugTrace("HandleCommand invoked without args or invalid args")
+    reportError("missing_args")
+    return
+  end
+  debugTrace("HandleCommand received command: " .. tostring(args.command))
+  if args.command == "build_gate" then
+    GateManager.CreateGate(args)
+  elseif args.command == "connect_gates" then
+    GateManager.ConnectGates(args)
+  elseif args.command == "disconnect_gates" then
+    GateManager.DisconnectGate(args)
+  elseif args.command == "mark_gate" or args.command == "unmark_gate" then
+    GateManager.MarkGateOnMap(args)
+  else
+    debugTrace("HandleCommand received unknown command: " .. tostring(args.command))
+    args.info = "UnknownCommand"
+    reportError(args)
+  end
+end
+
+function GateManager.Init()
+  getPlayerId()
+  ---@diagnostic disable-next-line: undefined-global
+  RegisterEvent("GateManager.HandleCommand", GateManager.HandleCommand)
+end
+
+Register_Require_With_Init("extensions.gate_manager.ui.gate_manager", GateManager, GateManager.Init)
+
+return GateManager
